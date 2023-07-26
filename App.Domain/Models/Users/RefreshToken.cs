76cd18ca6e74804a349036349d7c837cd6e15ea4
Prashant20985 <prashantsharma4931@gmail.com>
2023-07-26 19:56:57 +0200

    Added Application Extension

--- App.Domain/Models/Users/RefreshToken.cs
+++ App.Domain/Models/Users/RefreshToken.cs
@@ -1,14 +1,51 @@
-﻿namespace App.Domain.Models.Users;
-
-public class RefreshToken
+﻿namespace App.Domain.Models.Users
 {
-    public int Id { get; set; }
-    public string UserId { get; set; }
-    public User Users { get; set; }
-    public string Token { get; set; }
-    public DateTime Expires { get; set; } = DateTime.UtcNow.AddDays(7);
-    public bool IsExpired => DateTime.UtcNow >= Expires;
-    public DateTime? Revoked { get; set; }
-    public bool IsActive => Revoked == null && !IsExpired;
-}
+    /// <summary>
+    /// Represents a Refresh Token entity used for managing user authentication tokens.
+    /// </summary>
+    public class RefreshToken
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the Refresh Token.
+        /// </summary>
+        public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the user ID associated with this Refresh Token.
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the User object associated with this Refresh Token.
+        /// </summary>
+        public User Users { get; set; }
+
+        /// <summary>
+        /// Gets or sets the actual Refresh Token value.
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expiration date and time for the Refresh Token.
+        /// The default value is set to one week (7 days) from the current UTC time.
+        /// </summary>
+        public DateTime Expires { get; set; } = DateTime.UtcNow.AddDays(7);
+
+        /// <summary>
+        /// Gets a value indicating whether the Refresh Token has expired.
+        /// </summary>
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+
+        /// <summary>
+        /// Gets or sets the date and time when the Refresh Token was revoked.
+        /// This value is null if the token is active and has not been revoked.
+        /// </summary>
+        public DateTime? Revoked { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the Refresh Token is currently active.
+        /// It checks if the token has not been revoked and if it is not expired.
+        /// </summary>
+        public bool IsActive => Revoked == null && !IsExpired;
+    }
+}
---
+++ App.Persistence/Migrations/UserSchema/20230726173818_InitialUserSchemaCreate.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Persistence.Migrations.UserSchema
+{
+    /// <inheritdoc />
+    public partial class InitialUserSchemaCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "user");
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                schema: "user",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "user",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(225)", maxLength: 225, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(225)", maxLength: 225, nullable: false),
+                    IsAccountActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeleteUserComment = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    GuestUserComment = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppUserRoles",
+                schema: "user",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AppUserRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "user",
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppUserRoles_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "user",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RefreshToken",
+                schema: "user",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Expires = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Revoked = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshToken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RefreshToken_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "user",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUserRoles_RoleId",
+                schema: "user",
+                table: "AppUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshToken_UserId",
+                schema: "user",
+                table: "RefreshToken",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                schema: "user",
+                table: "Roles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                schema: "user",
+                table: "Users",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                schema: "user",
+                table: "Users",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppUserRoles",
+                schema: "user");
+
+            migrationBuilder.DropTable(
+                name: "RefreshToken",
+                schema: "user");
+
+            migrationBuilder.DropTable(
+                name: "Roles",
+                schema: "user");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "user");
+        }
+    }
+}
