b107a894ee9b5707da79a03090389518578969f1
zeynepsilasaglam <zaynap.saglam@gmail.com>
2023-10-25 15:03:43 +0200

    Added Endpoint to Add Student to group

--- App.API/Controllers/DentistTeacherController.cs
+++ App.API/Controllers/DentistTeacherController.cs
@@ -1,4 +1,5 @@
-﻿using App.Application.DentistTeacherOperations.Command.CreateGroup;
+﻿using App.Application.DentistTeacherOperations.Command.AddStudentToGroup;
+using App.Application.DentistTeacherOperations.Command.CreateGroup;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -8,7 +9,23 @@ namespace App.API.Controllers;
 [Authorize(Roles = "Dentist_Teacher_Researcher, Dentist_Teacher_Examiner")]
 public class DentistTeacherController : BaseController
 {
+    /// <summary>
+    /// Creates a new group.
+    /// </summary>
+    /// <param name="GroupName">The name of the group to create.</param>
+    /// <returns>An HTTP response indicating the result of the operation.</returns>
     [HttpPost("create-group")]
     public async Task<ActionResult> CreateGroup(string GroupName) => HandleOperationResult(
         await Mediator.Send(new CreateGroupCommand(GroupName, User.FindFirstValue(ClaimTypes.NameIdentifier))));
+
+
+    /// <summary>
+    /// Adds a student to a group.
+    /// </summary>
+    /// <param name="groupId">The identifier of the group to which the student will be added.</param>
+    /// <param name="studentId">The identifier of the student to add to the group.</param>
+    /// <returns>An HTTP response indicating the result of the operation.</returns>
+    [HttpPost("add-student")]
+    public async Task<ActionResult> AddStudentToGroup(Guid groupId, string studentId) => HandleOperationResult(
+        await Mediator.Send(new AddStudentToGroupCommand(groupId, studentId)));
 }
---
+++ App.Application/DentistTeacherOperations/Command/AddStudentToGroup/AddStudentToGroupHandler.cs
@@ -0,0 +1,40 @@
+﻿using App.Application.Core;
+using App.Domain.Models.Users;
+using App.Domain.Repository;
+using MediatR;
+
+namespace App.Application.DentistTeacherOperations.Command.AddStudentToGroup;
+
+/// <summary>
+/// Handles the addition of a student to a group based on the provided command.
+/// </summary>
+internal sealed class AddStudentToGroupHandler : IRequestHandler<AddStudentToGroupCommand, OperationResult<Unit>>
+{
+    private readonly IGroupRepository _groupRepository;
+
+    /// <summary>
+    /// Initializes a new instance of the AddStudentToGroupHandler class.
+    /// </summary>
+    /// <param name="groupRepository">The repository for managing group-related operations.</param>
+    public AddStudentToGroupHandler(IGroupRepository groupRepository) => _groupRepository = groupRepository;
+
+    /// <inheritdoc />
+    public async Task<OperationResult<Unit>> Handle(AddStudentToGroupCommand request, CancellationToken cancellationToken)
+    {
+        // Check if the student is already a member of the group.
+        var checkStudentAlreadyInGroup = await _groupRepository.GetStudentGroup(
+            request.StudentId, request.GroupId);
+
+        if (checkStudentAlreadyInGroup is not null)
+            return OperationResult<Unit>.Failure("Student already in group");
+
+        // Create a new student-group relationship using the provided information.
+        StudentGroup studentGroup = new(request.GroupId, request.StudentId);
+
+        // Add the newly created student-group relationship to the repository.
+        await _groupRepository.AddStudentToGroup(studentGroup);
+
+        // Return a success result with no specific data.
+        return OperationResult<Unit>.Success(Unit.Value);
+    }
+}
