9e4f8321009c6307ab6cbf4a9b5fac65b16ec475
zeynepsilasaglam <zaynap.saglam@gmail.com>
2023-10-25 15:15:10 +0200

    Added Endpoint to remove Student to group

---
+++ App.Application/DentistTeacherOperations/Command/RemoveStudentFromGroup/RemoveStudentFromGroupCommand.cs
@@ -0,0 +1,12 @@
+﻿using App.Application.Core;
+using App.Persistence.Attributes;
+using MediatR;
+
+namespace App.Application.DentistTeacherOperations.Command.RemoveStudentFromGroup;
+
+/// <summary>
+/// Represents a command to remove a student from a group.
+/// </summary>
+[UserContextUnitOfWork]
+public record RemoveStudentFromGroupCommand(Guid GroupId, string StudentId)
+    : IRequest<OperationResult<Unit>>;
