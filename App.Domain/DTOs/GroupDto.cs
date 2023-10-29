8b3bdf676076f4905489ad2d39147db1bb3c4251
harsh-S21901 <s21901@pjwstk.edu.pl>
2023-10-29 13:10:43 +0100

    Implemeted endpoint to get all groups with students list

--- App.Domain/DTOs/GroupDto.cs
+++ App.Domain/DTOs/GroupDto.cs
@@ -1,10 +1,22 @@
 ﻿namespace App.Domain.DTOs;
 
+/// <summary>
+/// Data transfer object representing a group.
+/// </summary>
 public class GroupDto
 {
+    /// <summary>
+    /// Gets or sets the Id of the user.
+    /// </summary>
     public Guid Id { get; set; }
 
+    /// <summary>
+    /// Gets or sets the groupname of the user.
+    /// </summary>
     public string GroupName { get; set; }
 
+    /// <summary>
+    /// Gets or sets a list of students names in which group is associated
+    /// </summary>
     public List<StudentDto> Students { get; set; }
 }
