daacad7dc8de8bb5d2bac2ae253b6fcec66ace68
harsh-S21901 <s21901@pjwstk.edu.pl>
2023-11-03 22:43:26 +0100

    Implemented endpoint tp updae user request status and comment

---
+++ App.Application/AdminOperations/Command/UpdateUserRequest/UpdateUserRequestStatusCommand.cs
@@ -0,0 +1,11 @@
+﻿using App.Application.Core;
+using App.Persistence.Attributes;
+using MediatR;
+
+namespace App.Application.AdminOperations.Command.UpdateUserRequest;
+
+/// <summary>
+/// Represents a command to update an request staus and admin comment of user user request.
+/// </summary>
+[UserContextUnitOfWork]
+public record UpdateUserRequestStatusCommand(Guid UserRequestId, string RequestStatus, string AdminComment) : IRequest<OperationResult<Unit>>;
