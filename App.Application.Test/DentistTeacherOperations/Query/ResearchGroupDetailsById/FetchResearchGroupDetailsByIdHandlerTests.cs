514275fb654a3e08682cd4e2b506318f130c8993
harsh-S21901 <s21901@pjwstk.edu.pl>
2024-02-29 12:17:57 +0100

    Implemented tests for research groups in dentist teacher operations of Application layer

---
+++ App.Application.Test/DentistTeacherOperations/Query/ResearchGroupDetailsById/FetchResearchGroupDetailsByIdHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using App.Application.DentistTeacherOperations.Query.ResearchGroupDetailsById;
+using App.Domain.DTOs;
+using Moq;
+
+namespace App.Application.Test.DentistTeacherOperations.Query.ResearchGroupDetailsById;
+
+public class FetchResearchGroupDetailsByIdHandlerTests : TestHelper
+{
+    private readonly FetchResearchGroupDetailsByIdHandler handler;
+    private readonly FetchResearchGroupDetailsByIdQuery query;
+
+    public FetchResearchGroupDetailsByIdHandlerTests()
+    {
+        handler = new FetchResearchGroupDetailsByIdHandler(researchGroupRepositoryMock.Object);
+        query = new FetchResearchGroupDetailsByIdQuery(Guid.NewGuid());
+    }
+
+    [Fact]
+    public async Task Handle_WithValidQuery_ShouldReturnResearchGroupDto()
+    {
+        // Arrange
+        var researchGroup = new ResearchGroupDto
+        {
+            GroupName = "Research Group",
+            Description = "Research Group Description"
+        };
+
+        researchGroupRepositoryMock.Setup(repo => repo.GetResearchGroupDetailsById(It.IsAny<Guid>()))
+            .ReturnsAsync(researchGroup);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsSuccessful);
+        Assert.NotNull(result.ResultValue);
+        Assert.Equal("Research Group", result.ResultValue.GroupName);
+        Assert.Equal("Research Group Description", result.ResultValue.Description);
+    }
+
+    [Fact]
+    public async Task Handle_WithInvalidQuery_ShouldReturnFailure()
+    {
+        // Arrange
+        researchGroupRepositoryMock.Setup(repo => repo.GetResearchGroupDetailsById(It.IsAny<Guid>()))
+            .ReturnsAsync(value: null);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.False(result.IsSuccessful);
+        Assert.Null(result.ResultValue);
+        Assert.Equal("Research group not found", result.ErrorMessage);
+    }
+}
---
+++ App.Application.Test/DentistTeacherOperations/Query/ResearchGroups/FetchResearchGroupsHandlerTests.cs
@@ -0,0 +1,63 @@
+﻿using App.Application.DentistTeacherOperations.Query.ResearchGroups;
+using App.Domain.DTOs;
+using MockQueryable.Moq;
+
+namespace App.Application.Test.DentistTeacherOperations.Query.ResearchGroups;
+
+public class FetchResearchGroupsHandlerTests : TestHelper
+{
+    private readonly FetchResearchGroupsHandler handler;
+    private readonly FetchResearchGroupsQuery query;
+
+    public FetchResearchGroupsHandlerTests()
+    {
+        handler = new FetchResearchGroupsHandler(researchGroupRepositoryMock.Object);
+        query = new FetchResearchGroupsQuery("Research group");
+    }
+
+    [Fact]
+    public async Task Handle_WithValidQuery_ShouldReturnResearchGroupDto()
+    {
+        // Arrange
+        var researchGroups = new List<ResearchGroupDto>
+        {
+            new ResearchGroupDto
+            {
+                GroupName = "Research group",
+                Description = "Research Group Description"
+            }
+        }.AsQueryable().BuildMockDbSet();
+
+        researchGroupRepositoryMock.Setup(repo => repo.GetAllResearchGroups())
+            .Returns(researchGroups.Object);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsSuccessful);
+        Assert.NotNull(result.ResultValue);
+        Assert.Equal("Research group", result.ResultValue.First().GroupName);
+        Assert.Equal("Research Group Description", result.ResultValue.First().Description);
+    }
+
+    [Fact]
+    public async Task Handle_WithInvalidQuery_ShouldReturnEmpty()
+    {
+        // Arrange
+        var researchGroups = new List<ResearchGroupDto>
+        {
+
+        }.AsQueryable().BuildMockDbSet();
+
+        researchGroupRepositoryMock.Setup(repo => repo.GetAllResearchGroups())
+            .Returns(researchGroups.Object);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.Empty(result.ResultValue);
+    }
+}
+
