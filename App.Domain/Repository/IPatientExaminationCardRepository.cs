54d457032ecc753938ac822cdbb4fd920a69f238
harsh-S21901 <s21901@pjwstk.edu.pl>
2024-06-22 14:21:00 +0200

    Added Repository layer for Patient Exmaination Card

--- App.Domain/Repository/IPatientExaminationCardRepository.cs
+++ App.Domain/Repository/IPatientExaminationCardRepository.cs
@@ -3,19 +3,91 @@ using App.Domain.Models.OralHealthExamination;
 
 namespace App.Domain.Repository;
 
+/// <summary>
+/// Repository for managing patient examination cards.
+/// </summary>
 public interface IPatientExaminationCardRepository
 {
+    /// <summary>
+    /// Gets a patient examination card data transfer object by its unique identifier.
+    /// </summary>
+    /// <param name="cardId">The unique identifier of the patient examination card.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the patient examination card data transfer object.</returns>
     Task<PatientExaminationCardDto> GetPatientExaminationCardDto(Guid cardId);
+
+    /// <summary>
+    /// Gets a patient examination card by its unique identifier.
+    /// </summary>
+    /// <param name="cardId">The unique identifier of the patient examination card.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the patient examination card.</returns>
     Task<Bewe> GetBeweByCardId(Guid cardId);
+
+    /// <summary>
+    /// Gets the API data associated with a patient examination card.
+    /// </summary>
+    /// <param name="cardId">The unique identifier of the patient examination card.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the API data.</returns>
     Task<API> GetAPIByCardId(Guid cardId);
+
+    /// <summary>
+    /// Gets the bleeding data associated with a patient examination card.
+    /// </summary>
+    /// <param name="cardId">The unique identifier of the patient examination card.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the bleeding data.</returns>
     Task<Bleeding> GetBleedingByCardId(Guid cardId);
+
+    /// <summary>
+    /// Gets the DMFT/DMFS data associated with a patient examination card.
+    /// </summary>
+    /// <param name="cardId">The unique identifier of the patient examination card.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the DMFT/DMFS data.</returns>
     Task<DMFT_DMFS> GetDMFT_DMFSByCardId(Guid cardId);
+
+    /// <summary>
+    /// Adds a patient examination card to the repository.
+    /// </summary>
+    /// <param name="patientExaminationCard">The patient examination card to add.</param>
     Task AddPracticePatientExaminationCard(PatientExaminationCard patientExaminationCard);
+
+    /// <summary>
+    /// Adds a risk factor assessment to the repository.
+    /// </summary>
+    /// <param name="riskFactorAssessment">The risk factor assessment to add.</param>
     Task AddRiskFactorAssessment(RiskFactorAssessment riskFactorAssessment);
+
+    /// <summary>
+    /// Adds a patient examination result to the repository.
+    /// </summary>
+    /// <param name="patientExaminationResult">The patient examination result to add.</param>
     Task AddPatientExaminationResult(PatientExaminationResult patientExaminationResult);
+
+    /// <summary>
+    /// Adds an Bewe to the repository.
+    /// </summary>
+    /// <param name="bewe">The Bewe to add.</param>
     Task AddBewe(Bewe bewe);
+
+    /// <summary>
+    /// Adds an API to the repository.
+    /// </summary>
+    /// <param name="api">The API to add.</param>
     Task AddAPI(API api);
+
+    /// <summary>
+    /// Adds a bleeding to the repository.
+    /// </summary>
+    /// <param name="bleeding">The bleeding to add.</param>
     Task AddBleeding(Bleeding bleeding);
+
+    /// <summary>
+    /// Adds a DMFT/DMFS to the repository.
+    /// </summary>
+    /// <param name="dmft_dmfs">The DMFT/DMFS to add.</param>
     Task AddDMFT_DMFS(DMFT_DMFS dmft_dmfs);
+
+    /// <summary>
+    /// Deletes a patient examination card from the repository.
+    /// </summary>
+    /// <param name="cardId">The unique identifier of the patient examination card to delete.</param>
     Task DeletePatientExaminationCard(Guid cardId);
 }
