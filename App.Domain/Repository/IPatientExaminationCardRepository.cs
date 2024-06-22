632010e12b0bd6ac0dd492d66afc0dab90194127
harsh-S21901 <s21901@pjwstk.edu.pl>
2024-06-22 14:14:33 +0200

    Added Repository layer for Patient Exmaination Card

---
+++ App.Domain/Repository/IPatientExaminationCardRepository.cs
@@ -0,0 +1,21 @@
+﻿using App.Domain.DTOs.PatientDtos.Response;
+using App.Domain.Models.OralHealthExamination;
+
+namespace App.Domain.Repository;
+
+public interface IPatientExaminationCardRepository
+{
+    Task<PatientExaminationCardDto> GetPatientExaminationCardDto(Guid cardId);
+    Task<Bewe> GetBeweByCardId(Guid cardId);
+    Task<API> GetAPIByCardId(Guid cardId);
+    Task<Bleeding> GetBleedingByCardId(Guid cardId);
+    Task<DMFT_DMFS> GetDMFT_DMFSByCardId(Guid cardId);
+    Task AddPracticePatientExaminationCard(PatientExaminationCard patientExaminationCard);
+    Task AddRiskFactorAssessment(RiskFactorAssessment riskFactorAssessment);
+    Task AddPatientExaminationResult(PatientExaminationResult patientExaminationResult);
+    Task AddBewe(Bewe bewe);
+    Task AddAPI(API api);
+    Task AddBleeding(Bleeding bleeding);
+    Task AddDMFT_DMFS(DMFT_DMFS dmft_dmfs);
+    Task DeletePatientExaminationCard(Guid cardId);
+}
