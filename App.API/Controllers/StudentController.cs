13b4fe810010b92092c6d486a455155f710ed7f9
zeynepsilasaglam <zaynap.saglam@gmail.com>
2024-06-30 13:26:51 +0200

    Added endpoint to check the student eligibilty to take an exam

--- App.API/Controllers/StudentController.cs
+++ App.API/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using App.Application.StudentOperations.Query.StudentGroupDetails;
+﻿using App.Application.StudentExamOperations.StudentOperations.Query.ExamEligbility;
+using App.Application.StudentOperations.Query.StudentGroupDetails;
 using App.Application.StudentOperations.Query.StudentGroupsList;
 using App.Domain.DTOs;
 using Microsoft.AspNetCore.Authorization;
@@ -26,4 +27,12 @@ public class StudentController : BaseController
     public async Task<ActionResult<GroupWithExamsListDto>> GetStudentGroupDetails(Guid groupId) =>
         HandleOperationResult(await Mediator.Send(
             new FetchStudentGroupDetailsWithExamsQuery(User.FindFirstValue(ClaimTypes.NameIdentifier), groupId)));
+
+    /// <summary>
+    /// Gets the eligibility of the student to take the exam.
+    /// </summary>
+    /// <returns>True if student is eligible to take the exam, false otherwise.</returns>
+    [HttpGet("exam-eligibility/{examId}")]
+    public async Task<ActionResult<bool>> CheckExamEligibility(Guid examId) =>
+        HandleOperationResult(await Mediator.Send(new ExamEligibiltyQuery(examId, User.FindFirstValue(ClaimTypes.NameIdentifier))));
 }
