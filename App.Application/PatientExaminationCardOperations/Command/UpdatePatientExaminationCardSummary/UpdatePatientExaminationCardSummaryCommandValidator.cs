63bece9e0768c1a3129b9acb657b2183a3cdd198
zeynepsilasaglam <zaynap.saglam@gmail.com>
2024-07-13 12:13:10 +0200

    Added Enpoint to Update Patient Examination Card Summary

---
+++ App.Application/PatientExaminationCardOperations/Command/UpdatePatientExaminationCardSummary/UpdatePatientExaminationCardSummaryCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using App.Application.PatientExaminationCardOperations.CommanValidators;
+using FluentValidation;
+
+namespace App.Application.PatientExaminationCardOperations.Command.UpdatePatientExaminationCardSummary;
+
+/// <summary>
+/// Validator for Update Patient Examination Card Summary Command.
+/// </summary>
+public class UpdatePatientExaminationCardSummaryCommandValidator
+    : AbstractValidator<UpdatePatientExaminationCardSummaryCommand>
+{
+    public UpdatePatientExaminationCardSummaryCommandValidator()
+    {
+        RuleFor(x => x.CardId)
+            .NotEmpty().WithMessage("CardId is required.");
+
+        RuleFor(x => x.Summary)
+            .NotNull().WithMessage("Summary is required.")
+            .SetValidator(new SummaryValidator());
+    }
+}
